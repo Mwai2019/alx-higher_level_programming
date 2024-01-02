d9cfd2bdbcb883f23adfccd83598b252d417ac2e
mwai2019 <mwaimatembamk2019@gmail.com>
2024-01-02 19:47:40 +0200

    test-drive

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-
-"""Square Class.
-This module contains a class that defines a square.
-Usage Example:
-    Square = __import__('1-square').Square
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """Defines the blueprint of a square.
-    Attribute:
-        size: An integer indicating the size of the square object.
-    """
+    """Represent a square."""
 
     def __init__(self, size):
-        """An object constructor method.
-        Initiatilizes Square with size.
-        Arg:
-            size: A integer representing object size
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
         """
         self.__size = size
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,15 @@
+Python - Test-driven development 0x07.
+An explanation
+Tasks in this project directory help students meet the following learning goals:
+
+The reasons why learning Python is so amazing
+What is meant by an interactive assessment?
+Why it's important to take tests
+Writing Docstrings to Generate Tests
+How each module and function should have its own documentation
+What are the standard option flags needed to build tests?
+Methods for identifying edge cases.
+Highlighted source documents
+scripts in Python
+Python exercise cases
+View file specifications by clicking this link.
